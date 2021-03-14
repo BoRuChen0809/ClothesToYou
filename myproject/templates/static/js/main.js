fcdcac54d996a56592c46d4ce03d24df5b8a8c14
SHIELD890624 <SHIELD890624@outlook.com>
2021-03-14 11:58:54 +0800

    profile頁面增加tab標籤內容

--- myproject/templates/static/js/main.js
+++ myproject/templates/static/js/main.js
@@ -43,3 +43,22 @@
 			});
 
 })(jQuery);
+
+function clickHandle(evt, animalName) {
+	let i, tabcontent, tablinks;
+	// This is to clear the previous clicked content.
+	tabcontent = document.getElementsByClassName("tabcontent");
+	for (i = 0; i < tabcontent.length; i++) {
+	  tabcontent[i].style.display = "none";
+	}
+
+	// Set the tab to be "active".
+	tablinks = document.getElementsByClassName("tablinks");
+	for (i = 0; i < tablinks.length; i++) {
+	  tablinks[i].className = tablinks[i].className.replace(" active", "");
+	}
+
+	// Display the clicked tab and set it to active.
+	document.getElementById(animalName).style.display = "block";
+	evt.currentTarget.className += " active";
+}
