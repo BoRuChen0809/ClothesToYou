c2c1eb33fa62f3ffb1c16304bf0b39282b48789f
PoPoChen0809 <chenboru0124@gmail.com>
2021-03-17 18:37:08 +0800

    3/16 13:10 完成 change_profile 3/17 10:10 test change pwd 3/17 11:35 完成 change pwd 3/17 13:15 startapp supplier 3/17 supplier model

--- myproject/supplier/models.py
+++ myproject/supplier/models.py
@@ -3,13 +3,13 @@ from django.db import models
 # Create your models here.
 
 class Supplier(models.Model):
-    ID = models.CharField(max_length=8, default=None)
-    B_Name = models.CharField(max_length=100,default=None)      #公司名稱
+    ID = models.CharField(max_length=8, default=None)           #統編****************
+    B_Name = models.CharField(max_length=100,default=None)      #公司名稱****************
     BU_Name = models.CharField(max_length=50,default=None)      #負責人姓名
     Phone = models.CharField(max_length=12, default=None)
     Mail = models.EmailField(default=None)
     PWD = models.BinaryField()
     Salt = models.BinaryField()
-    Active = models.BooleanField(default=False)
+    Active = models.BooleanField(default=False)                 #**********************
     Address = models.CharField(max_length=100, default=None)
     Picture = models.ImageField(default=None)
