9c3e3e7b08d18e52a9d634c5f377c9c8359d273c
SHIELD Studio <SHIELD890624@outlook.com>
2021-03-17 16:04:57 +0800

    03/17 增加供應商首頁、登入、註冊頁面

---
+++ myproject/supplier/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+urlpatterns = [
+    path('', views.sindex),
+    path('sindex/', views.sindex, name='sindex'),
+    path('become_partner/', views.become_partner, name='become_partner'),
+    path('slogin/', views.slogin, name='slogin'),
+]
--- myproject/supplier/views.py
+++ myproject/supplier/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def sindex(request):
+    return render(request, 'supplier_index.html')
+
+def become_partner(request):
+    return render(request, 'supplier_become_partner.html')
+
+def slogin(request):
+    return render(request, 'supplier_login.html')
