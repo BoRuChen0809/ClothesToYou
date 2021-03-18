2f3d8b68f63a4db0cc2f10ad7a7fe8dfbbf68560
PoPoChen0809 <chenboru0124@gmail.com>
2021-03-18 16:17:51 +0800

    3/16 13:10 完成 change_profile 3/17 10:10 test change pwd 3/17 11:35 完成 change pwd 3/17 13:15 startapp supplier 3/17 supplier model 3/18 10:45 改user 3/18 16:15 supplier register

--- main.py
+++ main.py
@@ -7,10 +7,33 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def check_id(str):
+    list = [1, 2, 1, 2, 1, 2, 4, 1]
+    serial_list = []
+    new_list = []
 
+    for c in str:
+        serial_list.append(int(c))
+        if not c.isnumeric():
+            return True
+            break
+
+    if len(serial_list) != 8:
+        return True
+
+    for i in range(8):
+        temp = serial_list[i] * list[i]
+        new_list.append(int(temp / 10) + int(temp % 10))
+
+    s = sum(new_list)
+    if s % 10 != 0 or (serial_list[6] == 7 and (s - new_list[6] % 10 == 0 or s - new_list[6] % 10 == 9)):
+        return True
+
+    return False
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    print(check_id("04595257"))
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- myproject/supplier/views.py
+++ myproject/supplier/views.py
@@ -1,5 +1,6 @@
 import re
 
+import bcrypt
 from django.shortcuts import render
 
 # Create your views here.
@@ -10,18 +11,53 @@ def s_index(request):
     return render(request, 'supplier_index.html')
 
 def become_partner(request):
-    if request.POTS:
-        warn_list = []
-        id = request.POTS['company_id']
-        c_name = request.POTS['company_name']  # 公司名稱****************
+    if request.POST:
+        warning_list = []
 
-        principal = request.POTS['supplier_name']  # 負責人姓名
-        phone = request.POTS['supplier_phone']
-        mail = request.POTS['supplier_email']
-        pwd = request.POTS['supplier_password']
-        check_pwd = request.POTS['password_check']
-        address = request.POTS['company_address']
-        #Picture = request.POTS['']
+        id = request.POST['company_id']
+        if check_id(id):
+            warning_list.append("統編有誤")
+
+        c_name = request.POST['company_name']  # 公司名稱****************
+        if check_name(c_name):
+            warning_list.append("公司名稱有誤")
+
+        principal = request.POST['supplier_name']  # 負責人姓名
+        if check_name(principal):
+            warning_list.append("姓名有誤")
+
+        phone = request.POST['supplier_phone']
+        if check_phone(phone):
+            warning_list.append("電話號碼有誤")
+
+        mail = request.POST['supplier_email']
+        if check_email(mail):
+            warning_list.append("電子郵件有誤")
+
+        pwd = request.POST['supplier_password']
+        if check_password(pwd):
+            warning_list.append("密碼有誤")
+
+        check_pwd = request.POST['password_check']
+        if check_pwd_match(pwd,check_pwd):
+            warning_list.append("確認密碼有誤")
+
+        address = request.POST['company_address']
+        if check_name(address):
+            warning_list.append("通訊地有誤")
+        #Picture = request.POST['']
+
+        if len(warning_list) > 0:
+            context = {'warning': warning_list}
+            return render(request, 'supplier_become_partner.html', context)
+        else:
+            salt,pwd = hashpwd(pwd)
+            supplier = Supplier(ID=id,C_Name=c_name,Principal=principal,Phone=phone,Mail=mail,PWD=pwd,Salt=salt,Active=False,Address=address)
+            supplier.save()
+
+            return render(request, 'register_succeed.html')
+
+
         return render(request, 'register_succeed.html')
     return render(request, 'supplier_become_partner.html')
 
@@ -33,8 +69,36 @@ def sprofile(request):
 
 
 
+def hashpwd(pwd):
+    salt = bcrypt.gensalt()
+    hashed_pwd = bcrypt.hashpw(pwd.encode('utf-8'), salt)
+    return salt,hashed_pwd
 
 #有問題rerturn True
+
+def check_id(str):
+    list = [1, 2, 1, 2, 1, 2, 4, 1]
+    serial_list = []
+    new_list = []
+
+    for c in str:
+        serial_list.append(int(c))
+        if not c.isnumeric():
+            return True
+            break
+
+    if len(serial_list) != 8:
+        return True
+
+    for i in range(8):
+        temp = serial_list[i] * list[i]
+        new_list.append(int(temp / 10) + int(temp % 10))
+
+    s = sum(new_list)
+    if s % 10 != 0 or (serial_list[6] == 7 and (s - new_list[6] % 10 == 0 or s - new_list[6] % 10 == 9)):
+        return True
+
+    return False
 def check_name(str):
     if str == "":
         return True
