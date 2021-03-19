68d57741612e95c892c7f7f059cbf6530cb9a24b
PoPoChen0809 <chenboru0124@gmail.com>
2021-03-19 12:04:16 +0800

    3/16 13:10 完成 change_profile 3/17 10:10 test change pwd 3/17 11:35 完成 change pwd 3/17 13:15 startapp supplier 3/17 supplier model 3/18 10:45 改user 3/18 16:15 supplier register 3/18 16:35 完成 supplier register 3/18 20:40 supplier login 3/19 12:00 完成 supplier login

--- myproject/supplier/models.py
+++ myproject/supplier/models.py
@@ -12,7 +12,7 @@ class Supplier(models.Model):
     Salt = models.BinaryField()
     Active = models.BooleanField(default=False)                 #**********************
     Address = models.CharField(max_length=100, default=None)
-    Picture = models.ImageField(default=None,blank=True)
+    Picture = models.ImageField(upload_to='brand/',default=None,blank=True)
 
 '''
 class Product(models.Model):
