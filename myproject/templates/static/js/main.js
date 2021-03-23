1e20c6f4bd6ed08b3d2bfeb0a0df9afb4f5a5b96
SHIELD Studio <SHIELD890624@outlook.com>
2021-03-23 18:34:54 +0800

    [03/23 18:33] 更改供應商圖像顯示及上傳方式

--- myproject/templates/static/js/main.js
+++ myproject/templates/static/js/main.js
@@ -62,21 +62,3 @@ function clickHandle(evt, animalName) {
 	document.getElementById(animalName).style.display = "block";
 	evt.currentTarget.className += " active";
 }
-
-//const UPLOAD_BUTTON = document.getElementById("upload-button");
-const FILE_INPUT = document.querySelector("input[type=file]");
-const AVATAR = document.getElementById("avatar");
-
-//UPLOAD_BUTTON.addEventListener("click", () => FILE_INPUT.click());
-
-FILE_INPUT.addEventListener("change", event => {
-  const file = event.target.files[0];
-
-  const reader = new FileReader();
-  reader.readAsDataURL(file);
-
-  reader.onloadend = () => {
-    AVATAR.setAttribute("aria-label", file.name);
-    AVATAR.style.background = `url(${reader.result}) center center/cover`;
-  };
-});
