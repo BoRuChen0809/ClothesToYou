d57d20131b32aa486f384ffdf9f0e87f064cbcd6
PoPoChen0809 <chenboru0124@gmail.com>
2021-03-23 20:56:03 +0800

    3/23 20:55 完成 supplier profile img顯示

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -13,12 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+
+from . import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include('user.urls')),
     path('supplier/', include('supplier.urls')),
-
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
