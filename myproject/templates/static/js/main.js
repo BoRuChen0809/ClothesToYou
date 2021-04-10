226c2a336e6046d30d363f96b780156e30293a24
SHIELD Studio <SHIELD890624@outlook.com>
2021-04-10 22:04:05 +0800

    [04/10 22:03] html source code 排版

--- myproject/templates/static/js/main.js
+++ myproject/templates/static/js/main.js
@@ -69,11 +69,3 @@ $(document).ready(function() {
         $("." + inputValue).toggle();
     });
 });
-
-new Drift(document.querySelector('.drift-demo-trigger'), {
-  paneContainer: document.querySelector('.details'),
-  inlinePane: 769,
-  inlineOffsetY: -85,
-  containInline: true,
-  hoverBoundingBox: true
-});
