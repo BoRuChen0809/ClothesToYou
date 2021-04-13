e3f0976a3cde48ef98650c947786bbab4feeeb70
SHIELD Studio <SHIELD890624@outlook.com>
2021-04-13 12:20:09 +0800

    [04/13 12:19] 修復排版

--- myproject/templates/static/js/main.js
+++ myproject/templates/static/js/main.js
@@ -76,11 +76,11 @@ var i;
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
+    var ccontent = this.nextElementSibling;
+    if (ccontent.style.display === "block") {
+      ccontent.style.display = "none";
     } else {
-      content.style.display = "block";
+      ccontent.style.display = "block";
     }
   });
 }
