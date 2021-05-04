9b5c7d0fbdebcac8b0939f08dbbd4d1a1d6395a0
SHIELD Studio <SHIELD890624@outlook.com>
2021-05-04 20:33:55 +0800

    [05/04 20:33] improve product/detail page

--- myproject/templates/static/js/main.js
+++ myproject/templates/static/js/main.js
@@ -69,3 +69,11 @@ $(document).ready(function() {
         $("." + inputValue).toggle();
     });
 });
+
+// Change image on thumbnail click.
+var thumbs = $(".icon-images").find("img");
+thumbs.click(function() {
+  var src = $(this).attr("src");
+  var dp = $(".display-img");
+  dp.attr("src", src);
+});
